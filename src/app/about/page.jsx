af653d8e66fc099cda50139afe463fd5adc57d75
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-03 14:00:16 +0500

    add page.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+function About() {
+    return (
+        <>
+            <h1 className=" text-[50px] text-purple-800 font-bold underline text-center">
+                App Router About Page
+            </h1>
+
+        </>
+    );
+}
+
+export default About;
