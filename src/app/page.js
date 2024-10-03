4b29ae32e3e08cef5ab2b0b4b1a2247e5b17aeca
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-03 14:00:51 +0500

    add page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1>
-        App Router
+      <h1 className=" text-[50px] text-purple-800 font-bold underline text-center">
+        App Router Home Page
       </h1>
     </>
   );
