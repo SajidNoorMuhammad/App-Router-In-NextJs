8b1bdd3ef112ba7369834e749746b2a0cc7a3a92
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-03 14:00:32 +0500

    add page.

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,12 @@
+function Contact() {
+    return (
+        <>
+            <h1 className=" text-[50px] text-purple-800 font-bold underline text-center">
+                App Router Contact Page
+            </h1>
+
+        </>
+    );
+}
+
+export default Contact;
